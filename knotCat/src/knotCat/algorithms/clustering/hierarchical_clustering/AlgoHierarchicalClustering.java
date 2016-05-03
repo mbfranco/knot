003651645d9017d0c0d0b5d8a468bb4da6da127a
mbfranco <miguelbfranco@gmail.com>
2016-05-03 08:53:43 +0100

    merging bigger nodes bug solved

--- knotCat/src/knotCat/algorithms/clustering/hierarchical_clustering/AlgoHierarchicalClustering.java
+++ knotCat/src/knotCat/algorithms/clustering/hierarchical_clustering/AlgoHierarchicalClustering.java
@@ -23,6 +23,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -64,7 +65,7 @@ public class AlgoHierarchicalClustering {
 	Set<BitArray> clusterControl = new HashSet<BitArray>();
 
 	// parameter
-	private double maxDistance =0;  // maximum distance allowed for merging two clusters
+	private double maxDistance = 0;  // maximum distance allowed for merging two clusters
 
 	// list of clusters
 	List<ClusterWithMean> clusters = null;
@@ -77,8 +78,6 @@ public class AlgoHierarchicalClustering {
 
 	/* The distance function to be used for clustering */
 	private DistanceFunction distanceFunction = null;
-
-	private ArrayList<String> names;
 
 	/**
 	 * Default constructor
@@ -104,7 +103,7 @@ public class AlgoHierarchicalClustering {
 		this.distanceFunction = distanceFunction;
 
 		// create an empty list of clusters
-		clusters = new ArrayList<ClusterWithMean>();
+		clusters = new LinkedList<ClusterWithMean>();
 
 		this.browser = browser;
 
@@ -136,7 +135,7 @@ public class AlgoHierarchicalClustering {
 		for(int index = 0; index < clusters.size(); index++){
 			for(BitArray ba : clusters.get(index).getVectors()){
 				if(!clusterControl.contains(ba)){
-					ClusterKnot ck = new ClusterKnot(clusters.get(index).getNames(), maxDistance+100, clusters.get(index).getVectors().get(0), clusters.get(index).getAtoms());
+					ClusterKnot ck = new ClusterKnot(clusters.get(index).getNames(), maxDistance, clusters.get(index).getVectors().get(0), clusters.get(index).getAtoms());
 					browser.getFinalCluster().add(ck);
 					clusterControl.add(ba);
 				}
@@ -149,56 +148,6 @@ public class AlgoHierarchicalClustering {
 		// return the clusters
 		return clusters;
 	}
-
-	/**
-	 * Run the algorithm only for the features.
-	 * @param inputFile an input file containing vectors of BitArrays
-	 * @param maxDistance  the maximum distance allowed for merging two clusters, i.e. the threshold
-	 * @param distanceFunction euclidean or hamming
-	 * @param browser this Browser
-	 * @return a list of Clusters 
-	 * @throws IOException exception if error while reading the file
-	 */
-	//	public List<ClusterWithMean> runOnlyFeaturesAlgorithm(String inputFile, double maxDistance, DistanceFunction distanceFunction, Browser browser) throws NumberFormatException, IOException {
-	//		// record start time
-	//		startTimestamp = System.currentTimeMillis();
-	//		
-	//		// save the parameter
-	//		this.maxDistance = maxDistance;
-	//		
-	//		// save the distance function
-	//		this.distanceFunction = distanceFunction;
-	//		
-	//		// create an empty list of clusters
-	//		clusters = new ArrayList<ClusterWithMean>();
-	//		
-	//		for(Knot k : browser.getKnotList()){
-	//			BitArray theVector = k.getFeatures().copy();
-	//			
-	//			// Initially we create a cluster for each vector
-	//			ClusterWithMean cluster = new ClusterWithMean(theVector.length());
-	//			cluster.addVector(theVector);
-	//			cluster.setMean(theVector.copy());
-	//			clusters.add(cluster);
-	//		}
-	//
-	//		// (2) Loop to combine the two closest clusters into a bigger cluster
-	//		// until no clusters can be combined.
-	//		boolean changed = false;
-	//		do {
-	//			// merge the two closest clusters
-	//			changed = mergeTheClosestCluster();
-	//			// record memory usage
-	//			MemoryLogger.getInstance().checkMemory();
-	//		} while (changed);
-	//
-	//		// record end time
-	//		endTimestamp = System.currentTimeMillis();
-	//		
-	//		// return the clusters
-	//		return clusters;
-	//	}
-
 
 	/**
 	 * Merge the two closest clusters in terms of distance.
@@ -237,13 +186,13 @@ public class AlgoHierarchicalClustering {
 
 
 		//Add the clusters to the dendrogram (browser.finalCluster), before the distance is lost and the clusters are merged
-		
+
 		//If it is a Leaf...
 		if(clusterToMerge1.getVectors().size() == 1){
-			
+
 			//...merging with a Leaf
 			if(clusterToMerge2.getVectors().size() == 1){
-				
+
 				//create the node that merges the two leaves
 				List<FinalCluster> branches = new ArrayList<FinalCluster>();
 				Node newNode = new Node(minClusterDistance, branches);		
@@ -252,7 +201,7 @@ public class AlgoHierarchicalClustering {
 				List<String> names = clusterToMerge1.getNames();
 				Map<Integer, BitArray> atomFeatures = clusterToMerge1.getAtoms();
 				ClusterKnot branch = new ClusterKnot(names, 0, clusterToMerge1.getVectors().get(0), atomFeatures);
-				
+
 				//add the first branch to the newNode
 				newNode.getBranches().add(branch);
 				newNode.setFeatures(clusterToMerge1.getVectors().get(0));
@@ -262,7 +211,7 @@ public class AlgoHierarchicalClustering {
 				names = clusterToMerge2.getNames();
 				atomFeatures = clusterToMerge2.getAtoms();
 				branch = new ClusterKnot(names, 0, clusterToMerge2.getVectors().get(0), atomFeatures);
-				
+
 				//add the second branch to the newNode
 				newNode.getBranches().add(branch);
 				newNode.setFeatures(newNode.getFeatures().or(clusterToMerge2.getVectors().get(0)));
@@ -274,10 +223,10 @@ public class AlgoHierarchicalClustering {
 				clusterControl.add(clusterToMerge2.getVectors().get(0));
 
 				//currentNode.setFeatures(clusterToMerge1.getVectors().get(0).or(clusterToMerge2.getVectors().get(0)));
-				
-				browser.getFinalCluster().add(newNode);
+
+				browser.getFinalCluster().add(currentNode);
 			}
-			
+
 			//...merging with a Node (clusterToMerge2.getVectors().size() == 2)
 			else{
 
@@ -297,7 +246,7 @@ public class AlgoHierarchicalClustering {
 				List<String> names = clusterToMerge1.getNames();
 				Map<Integer, BitArray> atomFeatures = clusterToMerge1.getAtoms();
 				ClusterKnot branch = new ClusterKnot(names, 0, clusterToMerge1.getVectors().get(0), atomFeatures);
-				
+
 				//add the leaf to the newNode
 				newNode.getBranches().add(branch);
 
@@ -315,11 +264,11 @@ public class AlgoHierarchicalClustering {
 		}
 
 		//If it is a Node...
-		else if(clusterToMerge1.getVectors().size() == 2){
-			
+		else if(clusterToMerge1.getVectors().size() >= 2){
+
 			//...merging with a Leaf
 			if(clusterToMerge2.getVectors().size() == 1){
-				
+
 				//create the node that merges the two leaves
 				List<FinalCluster> branches = new ArrayList<FinalCluster>();
 				Node newNode = new Node(minClusterDistance, branches);		
@@ -336,7 +285,7 @@ public class AlgoHierarchicalClustering {
 				List<String> names = clusterToMerge2.getNames();
 				Map<Integer, BitArray> atomFeatures = clusterToMerge2.getAtoms();
 				ClusterKnot branch = new ClusterKnot(names, 0, clusterToMerge2.getVectors().get(0), atomFeatures);
-				
+
 				//add the leaf to the newNode
 				newNode.getBranches().add(branch);
 
@@ -351,10 +300,10 @@ public class AlgoHierarchicalClustering {
 
 				browser.getFinalCluster().add(index, newNode);
 			}
-			
+
 			//...merging with a Node
 			else{
-				
+
 				//create the node that merges the two leaves
 				List<FinalCluster> branches = new ArrayList<FinalCluster>();
 				Node newNode = new Node(minClusterDistance, branches);		
@@ -362,13 +311,13 @@ public class AlgoHierarchicalClustering {
 				//add the first Branch to the newNode
 				currentNode = searchForNodeInTheDendrogram(clusterToMerge1.getNames());
 				newNode.getBranches().add(currentNode);
-				
+
 				//add the first Branch to the newNode
 				Node currentNode2 = searchForNodeInTheDendrogram(clusterToMerge2.getNames());
 				newNode.getBranches().add(currentNode2);
-				
-//				//Add knot in the same index that the previous was deleted
-//				int index = browser.getFinalCluster().indexOf(currentNode);
+
+				//				//Add knot in the same index that the previous was deleted
+				//				int index = browser.getFinalCluster().indexOf(currentNode);
 				browser.getFinalCluster().remove(currentNode);
 				browser.getFinalCluster().remove(currentNode2);
 
@@ -406,30 +355,30 @@ public class AlgoHierarchicalClustering {
 	 * @return the Node in the final tree cluster (dendrogram)
 	 */
 	private Node searchForNodeInTheDendrogram(List<String> names) {
-		
+
 		for(FinalCluster f : browser.getFinalCluster()){
 			if(f.getNames().containsAll(names)){
-				
+
 				return (Node)f;
 			}
 		}
 		return null;
-//		The next commented lines of code are correct, but unnecessary. The search is always for a root node
-//		for(FinalCluster f : browser.getFinalCluster()){
-//			Node n = new Node();
-//			if(f instanceof Node && f.getNames().containsAll(names)){
-//
-//				if(((Node) f).getBranches().get(0).getNames().containsAll(names)){
-//					n = searchForNodeInTheDendrogram(((Node) f).getBranches().get(0).getNames());
-//				}
-//				else if(((Node) f).getBranches().get(1).getNames().containsAll(names)){
-//					n = searchForNodeInTheDendrogram(((Node) f).getBranches().get(1).getNames());
-//				}
-//				return n;
-//			}
-//			//else go to the next cluster
-//		}
-//		return null;
+		//		The next commented lines of code are correct, but unnecessary. The search is always for a root node
+		//		for(FinalCluster f : browser.getFinalCluster()){
+		//			Node n = new Node();
+		//			if(f instanceof Node && f.getNames().containsAll(names)){
+		//
+		//				if(((Node) f).getBranches().get(0).getNames().containsAll(names)){
+		//					n = searchForNodeInTheDendrogram(((Node) f).getBranches().get(0).getNames());
+		//				}
+		//				else if(((Node) f).getBranches().get(1).getNames().containsAll(names)){
+		//					n = searchForNodeInTheDendrogram(((Node) f).getBranches().get(1).getNames());
+		//				}
+		//				return n;
+		//			}
+		//			//else go to the next cluster
+		//		}
+		//		return null;
 	}
 
 	/**
